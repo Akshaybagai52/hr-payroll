ee91bdbb48621af998e7afc915621572a59d837f
Akshay Bagai <akshaybagai52@gmail.com>
2024-08-11 18:30:35 +0530

    feat: added logo and social media icons

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,37 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import { Menu } from 'antd';
-import { services } from '../../data/serviceData';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { Menu } from 'antd'
+import { services } from '../../data/serviceData'
+import { Link, useLocation } from 'react-router-dom'
+import { FacebookFilled, LinkedinFilled,HomeFilled } from '@ant-design/icons'
 
 const Sidebar: React.FC = () => {
-  const location = useLocation();
-  const [currentKey, setCurrentKey] = useState('');
+  const location = useLocation()
+  const [currentKey, setCurrentKey] = useState('')
 
   useEffect(() => {
     if (location.pathname === '/') {
-      setCurrentKey('home');
+      setCurrentKey('home')
     } else {
-      const match = location.pathname.match(/\/service\/(\d+)/);
+      const match = location.pathname.match(/\/service\/(\d+)/)
       if (match) {
-        setCurrentKey(match[1]);
+        setCurrentKey(match[1])
       }
     }
-  }, [location]);
+  }, [location])
 
   return (
-    <div className="h-full bg-gray-800 text-white">
-      <Menu theme="dark" mode="inline" selectedKeys={[currentKey]}>
-        <Menu.Item key="home">
-          <Link to="/">Home</Link>
+    <div className='h-full bg-gray-800 text-white flex flex-col'>
+      <div className="p-4 flex items-center justify-center bg-gray-900">
+        <img src="https://i.ibb.co/3cxsmv3/logo.jpg" alt="Logo" className="w-32 h-auto" />
+      </div>
+      <Menu
+        theme='dark'
+        mode='inline'
+        selectedKeys={[currentKey]}
+        className='h-full relative'
+      >
+        <Menu.Item key='home'>
+          <Link to='/'>Home</Link>
         </Menu.Item>
         {services.map((service, index) => (
           <Menu.Item key={index.toString()}>
             <Link to={`/service/${index}`}>{service.title}</Link>
           </Menu.Item>
         ))}
+        <div className='absolute bottom-0 flex right-0 mr-3'>
+          <Menu.Item key='facebook' style={{padding: 0}}>
+            <Link
+              to={`https://www.facebook.com/share/j1rPVAxkJRhjHZhm/?mibextid=qi2Omg`}
+              target='_blank'
+            >
+              <FacebookFilled style={{ fontSize: 30 }} />
+            </Link>
+          </Menu.Item>
+          <Menu.Item key='linkedin' style={{padding: 0}}>
+            <Link to={`https://www.linkedin.com/company/nexgen-hr-solutions/`} target='_blank'>
+              <LinkedinFilled style={{ fontSize: 30 }} />
+            </Link>
+          </Menu.Item>
+        </div>
       </Menu>
     </div>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../components/sidebar/Sidebar';
 import AboutUs from '../components/about-us/AboutUs';
 import ServiceDetails from '../components/service-details/ServiceDetails';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Drawer, Button } from 'antd';
+import { MenuOutlined } from '@ant-design/icons';
 import useWindowSize from '../hooks/useWindowSize';
 
 const HomePage: React.FC = () => {
   const size = useWindowSize();
   const isMobile = size.width <= 768;
+  const [drawerVisible, setDrawerVisible] = useState(false);
+
+  const toggleDrawer = () => {
+    setDrawerVisible(!drawerVisible);
+  };
 
   return (
     <Router>
-      <div className="flex h-screen">
-        {!isMobile && <Sidebar />}
+      <div className="flex h-screen" >
+        {isMobile ? (
+          <>
+            <Button
+              icon={<MenuOutlined />}
+              onClick={toggleDrawer}
+              className="p-4 fixed top-3 right-3 z-10 "
+            />
+            <Drawer
+              placement="right"
+              closable={false}
+              onClose={toggleDrawer}
+              visible={drawerVisible}
+              bodyStyle={{ padding: 0 }}
+            >
+              <Sidebar />
+            </Drawer>
+          </>
+        ) : (
+          <Sidebar />
+        )}
         <div className="flex-1 overflow-y-auto">
           <Routes>
             <Route path="/" element={<AboutUs />} />
